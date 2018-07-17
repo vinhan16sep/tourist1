93ccabbce8f3e5c8e3794e9e5b0b8ad3437b8077
vinhan16sep <vinhan16sep@gmail.com>
2018-07-17 15:54:02 +0700

    implement share to facebook

--- application/views/templates/public_parts/master_header_view.php
+++ application/views/templates/public_parts/master_header_view.php
@@ -49,13 +49,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- Facebook SDK-->
 
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
+<script>
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=504743766549751&autoLogAppEvents=1';
+        js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=139238366917004&autoLogAppEvents=1';
         fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <section id="top-nav" class="container-fluid">
 	<div class="container">
